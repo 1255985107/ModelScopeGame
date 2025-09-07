7342ea6e187c233c2bc9eea7f247425c153c2f90
1255985107 <1255985107@qq.com>
2025-09-07 21:28:29 +0800

    Main Menu & Settings Menu

---
+++ Assets/Scripts/UI/AudioManager.cs
@@ -0,0 +1,239 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+/// <summary>
+/// 简单的音频管理器，负责播放音效和管理音频设置
+/// </summary>
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance { get; private set; }
+    
+    [Header("Audio Mixer")]
+    [SerializeField] private AudioMixer audioMixer;
+    
+    [Header("Audio Sources")]
+    [SerializeField] private AudioSource musicSource;
+    [SerializeField] private AudioSource sfxSource;
+    
+    [Header("Audio Clips")]
+    [SerializeField] private AudioClip buttonClickSound;
+    [SerializeField] private AudioClip buttonHoverSound;
+    
+    // Audio mixer parameter names (should match SettingsMenuManager)
+    private const string MUSIC_VOLUME_PARAM = "MusicVolume";
+    private const string SFX_VOLUME_PARAM = "SFXVolume";
+    
+    void Awake()
+    {
+        // Singleton pattern
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            InitializeAudioSources();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    void InitializeAudioSources()
+    {
+        // Create audio sources if not assigned
+        if (musicSource == null)
+        {
+            GameObject musicObj = new GameObject("MusicSource");
+            musicObj.transform.SetParent(transform);
+            musicSource = musicObj.AddComponent<AudioSource>();
+            musicSource.loop = true;
+            musicSource.playOnAwake = false;
+        }
+        
+        if (sfxSource == null)
+        {
+            GameObject sfxObj = new GameObject("SFXSource");
+            sfxObj.transform.SetParent(transform);
+            sfxSource = sfxObj.AddComponent<AudioSource>();
+            sfxSource.loop = false;
+            sfxSource.playOnAwake = false;
+        }
+        
+        // Assign audio mixer groups if available
+        if (audioMixer != null)
+        {
+            var musicGroup = audioMixer.FindMatchingGroups("Music");
+            var sfxGroup = audioMixer.FindMatchingGroups("SFX");
+            
+            if (musicGroup.Length > 0)
+                musicSource.outputAudioMixerGroup = musicGroup[0];
+            
+            if (sfxGroup.Length > 0)
+                sfxSource.outputAudioMixerGroup = sfxGroup[0];
+        }
+    }
+    
+    #region Music Methods
+    
+    /// <summary>
+    /// 播放背景音乐
+    /// </summary>
+    /// <param name="musicClip">音乐剪辑</param>
+    public void PlayMusic(AudioClip musicClip)
+    {
+        if (musicSource != null && musicClip != null)
+        {
+            musicSource.clip = musicClip;
+            musicSource.Play();
+        }
+    }
+    
+    /// <summary>
+    /// 停止背景音乐
+    /// </summary>
+    public void StopMusic()
+    {
+        if (musicSource != null)
+        {
+            musicSource.Stop();
+        }
+    }
+    
+    /// <summary>
+    /// 暂停背景音乐
+    /// </summary>
+    public void PauseMusic()
+    {
+        if (musicSource != null)
+        {
+            musicSource.Pause();
+        }
+    }
+    
+    /// <summary>
+    /// 恢复背景音乐
+    /// </summary>
+    public void ResumeMusic()
+    {
+        if (musicSource != null)
+        {
+            musicSource.UnPause();
+        }
+    }
+    
+    #endregion
+    
+    #region SFX Methods
+    
+    /// <summary>
+    /// 播放音效
+    /// </summary>
+    /// <param name="clip">音效剪辑</param>
+    public void PlaySFX(AudioClip clip)
+    {
+        if (sfxSource != null && clip != null)
+        {
+            sfxSource.PlayOneShot(clip);
+        }
+    }
+    
+    /// <summary>
+    /// 播放按钮点击音效
+    /// </summary>
+    public void PlayButtonClick()
+    {
+        PlaySFX(buttonClickSound);
+    }
+    
+    /// <summary>
+    /// 播放按钮悬停音效
+    /// </summary>
+    public void PlayButtonHover()
+    {
+        PlaySFX(buttonHoverSound);
+    }
+    
+    #endregion
+    
+    #region Volume Control Methods
+    
+    /// <summary>
+    /// 设置音乐音量
+    /// </summary>
+    /// <param name="volume">音量 (0-1)</param>
+    public void SetMusicVolume(float volume)
+    {
+        if (audioMixer != null)
+        {
+            // Convert 0-1 range to decibel range (-80 to 0)
+            float dbValue = volume > 0 ? Mathf.Log10(volume) * 20 : -80f;
+            audioMixer.SetFloat(MUSIC_VOLUME_PARAM, dbValue);
+        }
+        else if (musicSource != null)
+        {
+            musicSource.volume = volume;
+        }
+    }
+    
+    /// <summary>
+    /// 设置音效音量
+    /// </summary>
+    /// <param name="volume">音量 (0-1)</param>
+    public void SetSFXVolume(float volume)
+    {
+        if (audioMixer != null)
+        {
+            // Convert 0-1 range to decibel range (-80 to 0)
+            float dbValue = volume > 0 ? Mathf.Log10(volume) * 20 : -80f;
+            audioMixer.SetFloat(SFX_VOLUME_PARAM, dbValue);
+        }
+        else if (sfxSource != null)
+        {
+            sfxSource.volume = volume;
+        }
+    }
+    
+    /// <summary>
+    /// 获取音乐音量
+    /// </summary>
+    /// <returns>音量值 (0-1)</returns>
+    public float GetMusicVolume()
+    {
+        if (audioMixer != null)
+        {
+            float dbValue;
+            if (audioMixer.GetFloat(MUSIC_VOLUME_PARAM, out dbValue))
+            {
+                return dbValue <= -80f ? 0f : Mathf.Pow(10f, dbValue / 20f);
+            }
+        }
+        else if (musicSource != null)
+        {
+            return musicSource.volume;
+        }
+        return 1f;
+    }
+    
+    /// <summary>
+    /// 获取音效音量
+    /// </summary>
+    /// <returns>音量值 (0-1)</returns>
+    public float GetSFXVolume()
+    {
+        if (audioMixer != null)
+        {
+            float dbValue;
+            if (audioMixer.GetFloat(SFX_VOLUME_PARAM, out dbValue))
+            {
+                return dbValue <= -80f ? 0f : Mathf.Pow(10f, dbValue / 20f);
+            }
+        }
+        else if (sfxSource != null)
+        {
+            return sfxSource.volume;
+        }
+        return 1f;
+    }
+    
+    #endregion
+}
--- Assets/Scripts/UI/MainMenuManager.cs
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -1,18 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
+    [Header("Buttons")]
+    public Button startButton;
+    public Button settingsButton;
+    public Button quitButton;
+
+    [Tooltip("Settings Menu Manager")]
+    public SettingsMenuManager settingsMenuManager;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Assert(startButton != null, "Start Button is not assigned in the inspector");
+        Debug.Assert(settingsButton != null, "Settings Button is not assigned in the inspector");
+        Debug.Assert(quitButton != null, "Quit Button is not assigned in the inspector");
+        Debug.Assert(settingsMenuManager != null, "SettingsMenuManager is not assigned in the inspector");
+
+        startButton.onClick.AddListener(OnStartButtonClicked);
+        settingsButton.onClick.AddListener(OnSettingsButtonClicked);
+        quitButton.onClick.AddListener(OnQuitButtonClicked);
+        Debug.Log("Listeners added to buttons");
+
+        settingsMenuManager.CloseSettingsMenu();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void OnStartButtonClicked()
+    {
+        Debug.Log("Start Button Clicked");
+        // Add logic to start the game
+        SceneManager.LoadScene("Level1");
+    }
+
+    void OnSettingsButtonClicked()
+    {
+        Debug.Log("Settings Button Clicked");
+        settingsMenuManager.OpenSettingsMenu();
+    }
+
+    void OnQuitButtonClicked()
+    {
+        Debug.Log("Quit Button Clicked");
+        Application.Quit();
+    }
+
+    public void DisableButtons()
+    {
+        startButton.gameObject.SetActive(false);
+        settingsButton.gameObject.SetActive(false);
+        quitButton.gameObject.SetActive(false);
+    }
+
+    public void EnableButtons()
+    {
+        startButton.gameObject.SetActive(true);
+        settingsButton.gameObject.SetActive(true);
+        quitButton.gameObject.SetActive(true);
     }
 }
