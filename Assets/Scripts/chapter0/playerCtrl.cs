d33597c5e8f52570332cdab422332b43e5594cc0
Or4ng3 <imor4ng3@gmail.com>
2025-09-08 16:12:18 +0800

    chapter0: builded the scene, fragments, UI

---
+++ Assets/Scripts/chapter0/playerCtrl.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCtrl : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float jumpStrenth = 6f;
+    public float moveSpeed = 5f;
+
+    private float horizontalInput;
+    private bool isGrounded = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpStrenth);
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+    }
+}
